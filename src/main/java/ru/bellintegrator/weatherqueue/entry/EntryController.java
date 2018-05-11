db2c96f7e3b7a90082509967d6b4b97afc31ac20
Goriachev Maksim <MGoriachev@bell-main.bellintegrator.ru>
2018-05-11 17:53:55 +0300

    Добавил точку входа для вывода jsp

--- src/main/java/ru/bellintegrator/weatherqueue/entry/EntryController.java
+++ src/main/java/ru/bellintegrator/weatherqueue/entry/EntryController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class WelcomeController {
+public class EntryController {
     @RequestMapping(value = "/")
     public String welcome(Model model) {
         model.addAttribute("title", "Погодный брокер");
