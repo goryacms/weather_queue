2f11bb1dd913a7e6e21f5a3260dc786efa08615e
Goriachev Maksim <MGoriachev@bell-main.bellintegrator.ru>
2018-05-11 17:53:01 +0300

    Добавил бины

--- src/main/java/ru/bellintegrator/weatherqueue/Application.java
+++ src/main/java/ru/bellintegrator/weatherqueue/Application.java
@@ -7,10 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
+import java.util.Locale;
+
+@ImportResource("spring_mvc_config.xml")
 @SpringBootApplication
 public class Application {
 
@@ -25,6 +30,18 @@ public class Application {
         executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors() + 1);
         executor.setQueueCapacity(25);
         return executor;
+    }
+
+
+
+
+
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);
+        return sessionLocaleResolver;
     }
 
 
