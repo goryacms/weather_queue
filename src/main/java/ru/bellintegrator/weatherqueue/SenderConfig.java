ba05960eaa405946e142fa0a70128f28ddcc0738
Goriachev Maksim <MGoriachev@bell-main.bellintegrator.ru>
2018-06-04 18:33:03 +0300

    Добавил файл конфигурации для Jms

--- src/main/java/ru/bellintegrator/weatherqueue/SenderConfig.java
+++ src/main/java/ru/bellintegrator/weatherqueue/SenderConfig.java
@@ -1,40 +1,23 @@
 package ru.bellintegrator.weatherqueue;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
-
 import javax.jms.ConnectionFactory;
-
 
 @Configuration
 public class SenderConfig {
-//    @Value("${activemq.broker-url}")
-//    private String brokerUrl;
-//    @Value("${activemq.user}")
-//    private String userName;
-//    @Value("${activemq.password}")
-//    private String password;
-
-
-    /*
-     * Initial ConnectionFactory
-     */
-    @Bean
+    @Bean // Инициализация ConnectionFactory
     public ConnectionFactory connectionFactory(){
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-//        connectionFactory.setBrokerURL(brokerUrl);
-//        connectionFactory.setUserName(userName);
-//        connectionFactory.setPassword(password);
         return connectionFactory;
     }
 
-    @Bean // Serialize message content to json using TextMessage
+    @Bean // Сериализация содержимого сообщений в json
     public MessageConverter jacksonJmsMessageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
@@ -42,38 +25,12 @@ public class SenderConfig {
         return converter;
     }
 
-    /*
-     * Used for Sending Messages.
-     */
-    @Bean
+    @Bean // Используется для отправки сообщений
     public JmsTemplate jmsTemplate(){
         JmsTemplate template = new JmsTemplate();
         template.setMessageConverter(jacksonJmsMessageConverter());
         template.setConnectionFactory(connectionFactory());
+
         return template;
     }
-
-
-
 }
-
-//@Configuration
-//public class SenderConfig {
-//
-//    @Value("${activemq.broker-url}")
-//    private String brokerUrl;
-//
-//    @Bean
-//    public ActiveMQConnectionFactory activeMQConnectionFactory() {
-//        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
-//        activeMQConnectionFactory.setBrokerURL(brokerUrl);
-//
-//        return activeMQConnectionFactory;
-//    }
-//
-//    @Bean
-//    public JmsTemplate jmsTemplate() {
-//        return new JmsTemplate(activeMQConnectionFactory());
-//    }
-//
-//}
