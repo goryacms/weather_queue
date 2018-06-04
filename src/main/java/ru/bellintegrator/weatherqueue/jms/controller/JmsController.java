097a31d8f2076996aecac0e2c4def795188e0ac2
Goriachev Maksim <MGoriachev@bell-main.bellintegrator.ru>
2018-06-04 18:46:40 +0300

    Добавил javaDoc, убрал лишнее

--- src/main/java/ru/bellintegrator/weatherqueue/jms/controller/JmsController.java
+++ src/main/java/ru/bellintegrator/weatherqueue/jms/controller/JmsController.java
@@ -1,21 +1,11 @@
 package ru.bellintegrator.weatherqueue.jms.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Controller;
-
-
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.*;
 import ru.bellintegrator.weatherqueue.jms.model.Location;
 import ru.bellintegrator.weatherqueue.jms.service.JmsService;
-import ru.bellintegrator.weatherqueue.jms.model.Weather;
-
 import java.io.IOException;
 
 @Controller
@@ -27,23 +17,23 @@ public class JmsController {
         this.jmsService = jmsService;
     }
 
-    @RequestMapping(value = "/jms", method = RequestMethod.POST)
+    public JmsController() {
+    }
+
+    /**
+     * Метод для формирования и отправки jms-сообщения. Для получения параметров из формы используется POST
+     * @param model
+     * @param city
+     * @return
+     * @throws IOException
+     */
+    @PostMapping(value = "/jms")
     public String weatherData(Model model, @RequestBody String city) throws IOException {
         // Получить ответ с yahoo.weather.com
         Location weatherInCity = jmsService.loadByCity(city);
 
-
         // Сформировать JMS
         jmsService.sendMessage(weatherInCity);
-
-
-
-
-
-
-
-        //Weather weather2 = new Weather();
-        //jmsService.receiveMessage(weather2);
 
         return "redirect:/";
     }
