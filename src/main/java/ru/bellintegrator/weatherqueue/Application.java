ae493b7a6f06bba52369f73042c77c48d86efd02
Goriachev Maksim <MGoriachev@bell-main.bellintegrator.ru>
2018-06-04 18:30:46 +0300

    Унаследовал Application от SpringBootServletInitializer (это помогло решить проблему с http на WildFly)

--- src/main/java/ru/bellintegrator/weatherqueue/Application.java
+++ src/main/java/ru/bellintegrator/weatherqueue/Application.java
@@ -1,64 +1,31 @@
 package ru.bellintegrator.weatherqueue;
 
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.core.JmsMessageOperations;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
-
-
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-
-import javax.jms.ConnectionFactory;
 import java.util.Locale;
-
-//@ImportResource("spring_mvc_config.xml")
-
 
 @SpringBootApplication
 @EnableJms
-public class Application {
-    //public final static String WEATHER_MESSAGE_QUEUE = "weather-message-queue";
+public class Application extends SpringBootServletInitializer {
     public final static String WEATHER_ROOT_NAME = "query"; // Корневой элемент json-ответа с weather.yahoo.com
 
-    public static void main(String[] args) {
-//        SpringApplication app = new SpringApplication(Application.class);
-//        app.run(args);
-
-
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-
-
-//        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
-//        jmsTemplate.convertAndSend("sampleQueue", "Hello world!");
-
-
-        //JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
-        // Send a message with a POJO - the template reuse the message converter
-
-        //jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hello"));
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 
     @Bean
     public TaskExecutor controllerPool() {
@@ -74,28 +41,4 @@ public class Application {
         sessionLocaleResolver.setDefaultLocale(Locale.US);
         return sessionLocaleResolver;
     }
-
-
-
-//    // Бины для jms
-//    @Bean
-//    public JmsListenerContainerFactory<?> jmsFactory(//ActiveMQConnectionFactory connectionFactory,
-//                                                    ConnectionFactory connectionFactory,
-//                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
-//        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-//        configurer.configure(factory, connectionFactory);
-//        return factory;
-//    }
-//
-//
-//    // Сериализация контента сообщений в json
-//    @Bean
-//    public MessageConverter jacksonJmsMessageConverter() {
-//        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-//        converter.setTargetType(MessageType.TEXT);
-//        converter.setTypeIdPropertyName("_type");
-//        return converter;
-//    }
-
-
 }
