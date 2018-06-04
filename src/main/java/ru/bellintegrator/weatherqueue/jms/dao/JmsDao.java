3d683dc09d85232fc7e9c9052e0952c583684d37
Goriachev Maksim <MGoriachev@bell-main.bellintegrator.ru>
2018-06-04 18:43:12 +0300

    добавил JavaDoc

--- src/main/java/ru/bellintegrator/weatherqueue/jms/dao/JmsDao.java
+++ src/main/java/ru/bellintegrator/weatherqueue/jms/dao/JmsDao.java
@@ -1,5 +1,13 @@
 package ru.bellintegrator.weatherqueue.jms.dao;
 
+/**
+ * Dao для загрузки информации с yahoo.weather.com
+ */
 public interface JmsDao {
+    /**
+     * Загрузчик данных по наименованию города
+     * @param city
+     * @return
+     */
     String loadByCity(String city);
 }
